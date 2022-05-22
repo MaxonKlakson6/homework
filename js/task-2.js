9ad8e2727b547a353debb4bf784458e0a232fdb7
Maxim <mihalenko.maxim21@gmail.com>
2022-05-22 17:11:11 +0300

    Task-2 modified

--- js/task-2.js
+++ js/task-2.js
@@ -1,31 +1,36 @@
+const IsEven = (value) => {
+  return value % 2 === 0;
+};
+
+const makeCapitalLetter = (str) => {
+  const lowerCaseStr = str.toLowerCase();
+  return lowerCaseStr[0].toUpperCase() + lowerCaseStr.slice(1);
+};
+
 // task 1
 
 const incrementArrayNumbers = (numbersArray) => {
-  return numbersArray.map((number) => {
-    return number % 2 === 0 ? (number += 1) : (number -= 1);
-  });
+  return numbersArray.map((number) =>
+    IsEven(number) ? number + 1 : number - 1
+  );
 };
 
 // task 2
 
 const capitalizeArrayElements = (stringArray) => {
-  return stringArray.map((stringValue) => {
-    return stringValue[0].toUpperCase() + stringValue.slice(1).toLowerCase();
-  });
+  return stringArray.map((stringValue) => makeCapitalLetter(stringValue));
 };
 
 // task 3
 
 const deleteUsersById = (userList, idList) => {
-  return userList.filter(({ id }) => {
-    return !idList.includes(id);
-  });
+  return userList.filter(({ id }) => !idList.includes(id));
 };
 
 // task 4
 
 const isPolindrome = (word) => {
-  const reverseWord = Array.from(word).reverse().join("");
+  const reverseWord = [...word].reverse().join("");
 
   return reverseWord.toLowerCase() === word.toLowerCase();
 };
@@ -37,17 +42,12 @@ const handlingUsers = (userList) => {
   const women = [];
 
   userList.forEach(({ gender, first_name, last_name, ...otherFields }) => {
-    gender.toLowerCase() === "male"
-      ? men.push({
-          gender,
-          fullName: `${first_name} ${last_name}`,
-          ...otherFields,
-        })
-      : women.push({
-          gender,
-          fullName: `${first_name} ${last_name}`,
-          ...otherFields,
-        });
+    const user = {
+      gender,
+      fullName: `${first_name} ${last_name}`,
+      ...otherFields,
+    };
+    gender.toLowerCase() === "male" ? men.push(user) : women.push(user);
   });
 
   return { men, women };
@@ -56,16 +56,14 @@ const handlingUsers = (userList) => {
 // task 6
 
 const deleteFields = (releses) => {
-  return releses.map(({ id, title }) => {
-    return { id, title };
-  });
+  return releses.map(({ id, title }) => ({ id, title }));
 };
 
 // task 7
 
 const filterByRating = (releses) => {
-  const neededRating = "5.0";
+  const neededRating = 5.0;
   return releses.filter(({ rating }) => {
-    return `${rating.toString()}.0` === neededRating;
+    return rating === neededRating;
   });
 };
